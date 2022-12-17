0b4ed6efcb84afbc7149d2ebca626f671d69af7b
gameitthoi <lamdat3302@gmail.com>
2022-12-17 20:43:01 +0700

    update pagination (3)

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -81,11 +81,7 @@
                                         <span class="bg-primary text-white relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium border border-gray-300 cursor-default leading-5">{{ $page }}</span>
                                     </span>
                                     <!-- hiển thị các trang trong khoảng quy định -->
-                                @elseif($page == $paginator->lastPage())
-                                <a href="{{ $url }}" class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 hover:text-gray-500 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150" aria-label="{{ __('Go to page :page', ['page' => $page]) }}">
-                                    <i class="fa-solid fa-angles-right"></i>
-                                </a>
-                                @elseif($page > $limit_page + 1 && $paginator->currentPage() <= $limit_page)
+                                @elseif($page > $limit_page + 1 && $paginator->currentPage() <= $limit_page && $page != $paginator->lastPage())
                                     <!-- không làm gì cả, không hiện cả trang đó ra lun -->
                                 @elseif($page <= $paginator->currentPage() + $limit_page && $page >= $paginator->currentPage() - $limit_page)
                                     <a href="{{ $url }}" class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 hover:text-gray-500 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150" aria-label="{{ __('Go to page :page', ['page' => $page]) }}">
